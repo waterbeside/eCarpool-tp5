f9320a3a70c35edde250319f82a413f9571552c5
waterbeside <waterbeside@163.com>
2018-04-17 16:21:22 +0800

    feat: 添加AdminLog模型

---
+++ application/common/model/AdminLog.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\common\model;
+
+use think\Db;
+use think\Model;
+
+class AdminLog extends Model
+{
+  public function add($desc='',$status=2,$uid=0){
+    $request = request();
+    $data['uid'] = $uid;
+    $data['ip'] = $request->ip();
+    // $data['path'] = $request->path();
+    $isAjaxShow =  $request->isAjax() ? " (Ajax)" : "";
+    $data['type'] = $request->method()."$isAjaxShow";
+    $data['route']= $request->module().'/'.$request->controller().'/'.$request->action();
+    $data['query_string'] = $request->query();
+    $data['description'] = $desc;
+    $data['status'] = $status;
+    $data['time'] = time();
+    $this->insert($data);
+  }
+}
