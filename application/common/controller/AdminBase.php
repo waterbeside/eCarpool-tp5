d52795cc1241cf6866b1630a3646b4ddbc6ed298
waterbeside <waterbeside@163.com>
2018-04-17 13:33:23 +0800

    feat: 新增jsonReturn方法

--- application/common/controller/AdminBase.php
+++ application/common/controller/AdminBase.php
@@ -139,4 +139,36 @@ class AdminBase extends Controller
       Db::name('admin_log')->insert($data);
     }
 
+
+
+
+    /**
+     * 返回json数据
+     * @param  integer $code    [状态码]
+     * @param  array $data    [主要数据]
+     * @param  string $message [描述]
+     * @param  array  $extra   [其它]
+     */
+  	public function jsonReturn($code, $data, $message = '',$extra = array()) {
+  		header('Access-Control-Allow-Origin: *');
+  		header('Access-Control-Allow-Headers:*');
+  		if($_SERVER['REQUEST_METHOD']=='OPTIONS'){
+  			exit;
+  		}
+      if(is_string($data)){
+        $message = $data;
+        $data = [];
+      }
+  		$data = array(
+  			'code'=>$code,
+  			'desc'=>$message,
+  			'data'=>$data,
+  			'date'=>date("Y-m-d H:i:s",time()),
+  			'extra'=>$extra
+  		);
+      return json($data);
+  		/*echo json_encode($data);
+  		exit;*/
+  	}
+
 }
