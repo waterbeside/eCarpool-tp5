a2ee268f8e3be395b004b25c88ee90e2b9fcf60a
waterbeside <waterbeside@163.com>
2018-05-17 10:41:59 +0800

    feat: 积分历史

--- application/admin/controller/ScoreHistory.php
+++ application/admin/controller/ScoreHistory.php
@@ -2,6 +2,7 @@
 namespace app\admin\controller;
 
 use app\score\model\History as HistoryModel;
+use app\score\model\Account as ScoreAccountModel;
 use app\carpool\model\User as CarpoolUserModel;
 use app\common\controller\AdminBase;
 use think\Db;
@@ -16,36 +17,122 @@ class ScoreHistory extends AdminBase
 
 
     /**
-     * 积分帐号
+     * 积分历史（所有用户）
      * @return mixed
      */
-    public function index($type=1,$account="",$keyword="",$page = 1,$pagesize = 20)
+    public function index($filter=NULL,$page = 1,$pagesize = 20)
     {
+        $map = [];
+        $map[] = ['is_delete','<>', 1];
+        // dump($filter);
+        if($filter['reason']){
+          $map[] = ['reason','=', $filter['reason']];
+        }
+        if($filter['time']){
+          $time_arr = explode(' ~ ',$filter['time']);
+          if(is_array($time_arr)){
+            $time_s = date('Y-m-d H:i:s',strtotime($time_arr[0]));
+            $time_e = date('Y-m-d H:i:s',strtotime($time_arr[1]));
+            $map[] = ['time', 'between time', [$time_s, $time_e]];
+          }
+        }
 
-      // dump(config("score.reason"));exit;
-      if(!$account){
-        $this->jsonReturn(-1,[],'lost account');
-      }
-      $map = [];
-      $map[] = ['is_delete','<>', 1];
+        $lists = HistoryModel::where($map)->order('time DESC, id DESC')->paginate($pagesize, false,  ['query'=>request()->param()]);
+        foreach ($lists as $key => $value) {
+          if($value['account_id']){
+            $userInfo = ScoreAccountModel::where(['id'=>$value['account_id']])->find();
+            $lists[$key]['account'] = $userInfo['carpool_account'] ? $userInfo['carpool_account'] : ($userInfo['phone'] ? $userInfo['carpool_account'] : $userInfo['identifier']);
+          }else if($value['carpool_account']){
+            $lists[$key]['account'] = $value['carpool_account'];
+          }
+        }
 
-      if( $type=='0' || $type=="score" ){
-        dump($type);
-        $map[] = ['indentifier','=', "$account"];
-      }else if($type=='2'||$type=="carpool"){ //拼车帐号列表
+        $reasons = config('score.reason');
 
-        $map[] = ['carpool_account','=', "$account"];
-      }
-      if ($keyword) {
-          $map[] = ['indentifier|carpool_account','like', "%{$keyword}%"];
-      }
-
-
-      $lists = HistoryModel::where($map)->order('time DESC')->paginate($pagesize, false,  ['query'=>request()->param()]);
-
-      return $this->fetch('index', ['lists' => $lists, 'keyword' => $keyword,'pagesize'=>$pagesize,'type'=>$type]);
+        $returnData = [
+          'lists' => $lists,
+          'filter' => $filter,
+          'pagesize'=>$pagesize,
+          'reasons'=>$reasons
+        ];
+        return $this->fetch('index', $returnData);
 
     }
+
+    /**
+     * 积分历史（指定用户）
+     * @return mixed
+     */
+    public function lists($type=1,$account=NULL,$account_id=NULL,$filter=NULL,$page = 1,$pagesize = 20)
+    {
+
+
+      $map = [];
+      $map[] = ['is_delete','<>', 1];
+      // dump($filter);
+      if($filter['reason']){
+        $map[] = ['reason','=', $filter['reason']];
+      }
+      if($filter['time']){
+        $time_arr = explode(' ~ ',$filter['time']);
+        if(is_array($time_arr)){
+          $time_s = date('Y-m-d H:i:s',strtotime($time_arr[0]));
+          $time_e = date('Y-m-d H:i:s',strtotime($time_arr[1]));
+          $map[] = ['time', 'between time', [$time_s, $time_e]];
+        }
+      }
+
+      if( $type=='0' || $type=="score" ){ //直接从积分帐号取
+        if(!$account&&!$account_id){ //当account为空时，读取全部
+          $this->error("Lost account or account_id");
+        }
+        if($account_id){
+          $accountInfo = ScoreAccountModel::where(['id'=>$account_id])->find();
+        }else{
+          $accountInfo = ScoreAccountModel::where(['account'=>$account])->find();
+        }
+
+        if(!$accountInfo){
+          $this->error("账号不存在");
+        }
+        if($accountInfo['carpool_account']){
+          $userInfo = CarpoolUserModel::where(['loginname'=>$account])->find();
+        }
+        $map[] = ['account_id','=', $accountInfo['id']];
+      }else if($type=='2'||$type=="carpool"){ //从拼车帐号取
+        if(!$account){ //当account为空时，读取全部
+          $this->error("Lost account");
+        }
+        $accountInfo = ScoreAccountModel::where(['carpool_account'=>$account])->find();
+        $userInfo = CarpoolUserModel::where(['loginname'=>$account])->find();
+        if($accountInfo){
+          $map[] = ['account_id','=', $accountInfo['id']];
+        }else{
+          $map[] = ['carpool_account','=', $account];
+        }
+      }
+
+      $lists = HistoryModel::where($map)->order('time DESC, id DESC')->paginate($pagesize, false,  ['query'=>request()->param()]);
+
+      $auth['admin/ScoreHistory/edit'] = $this->checkActionAuth('admin/ScoreHistory/edit');
+      $auth['admin/ScoreHistory/delete'] = $this->checkActionAuth('admin/ScoreHistory/delete');
+      $reasons = config('score.reason');
+
+      $returnData = [
+        'lists' => $lists,
+        'filter' => $filter,
+        'pagesize'=>$pagesize,
+        'type'=>$type,
+        'auth'=>$auth,
+        'account'=>$account,
+        'reasons'=>$reasons
+      ];
+      return $this->fetch('lists', $returnData);
+
+    }
+
+
+
 
 
 
