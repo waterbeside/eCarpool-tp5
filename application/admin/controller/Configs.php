886a341ad197f50b25defd10f4c81dbdb1149738
waterbeside <waterbeside@163.com>
2018-06-01 15:36:15 +0800

    feat: 加入configs模型

---
+++ application/admin/controller/Configs.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\admin\controller;
+
+use think\facade\Env;
+use app\common\controller\AdminBase;
+use think\facade\Cache;
+use think\Db;
+
+/**
+ * 系统配置
+ * Class System
+ * @package app\admin\controller
+ */
+class Configs extends AdminBase
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+
+}
