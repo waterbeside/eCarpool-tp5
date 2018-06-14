0c5999b69253cdebbea0599eaac8da9c580a2c34
waterbeside <waterbeside@163.com>
2018-06-14 10:23:40 +0800

    feat: 加入ios审核开头设置

--- application/admin/controller/Configs.php
+++ application/admin/controller/Configs.php
@@ -5,6 +5,7 @@ use think\facade\Env;
 use app\common\controller\AdminBase;
 use think\facade\Cache;
 use think\Db;
+use my\RedisData;
 
 /**
  * 系统配置
@@ -16,6 +17,21 @@ class Configs extends AdminBase
     public function initialize()
     {
         parent::initialize();
+    }
+
+    public function ios_audit_switch(){
+      $redis = new RedisData();
+      $data = json_decode($redis->get("AUDIT_SETTING"),true);
+      if ($this->request->isPost()){
+        $val   = $this->request->post('val');
+        if(!$val){
+          $this->error("不能为空");
+        }
+        $redis->set('AUDIT_SETTING', $val);
+        $this->success("修改成功");
+      }else{
+        return $this->fetch('ios_audit_switch',['data'=>$data]);
+      }
     }
 
 
