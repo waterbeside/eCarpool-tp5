389a9dba41eb0381b04034c751dc95a03f3b488b
waterbeside <waterbeside@163.com>
2018-06-22 15:34:48 +0800

    opt: 显示采购价

--- application/score/validate/Goods.php
+++ application/score/validate/Goods.php
@@ -8,14 +8,17 @@ class Goods extends Validate
     protected $rule = [
       'name'  => 'require',
       'price'  => 'require|float',
+      'amount'  => 'require|float',
       'inventory'  => 'require',
     ];
 
     protected $message = [
         'name.require'  => '商品名不能为空',
-        'price.require'     => '价格不能为空',
+        'price.require'     => '兑换价格不能为空',
+        'amount.require'     => '采购价不能为空',
         'inventory.require'     => '库存不能为空',
         'price.float'     => '价格必须为数字',
+        'amount.float'     => '价格必须为数字',
         'inventory.number'     => '库存必须为数字',
     ];
 
