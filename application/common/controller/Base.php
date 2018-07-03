153719a7885ac0148a192a27810111528b9740b6
waterbeside <waterbeside@163.com>
2018-07-03 12:00:32 +0800

    opt: 取得请求的语言码

--- application/common/controller/Base.php
+++ application/common/controller/Base.php
@@ -16,16 +16,49 @@ use think\facade\Cache;
 class Base extends Controller
 {
 
-  public $systemConfig  = null;
-  public $language  = 'zh-cn';
+    public $systemConfig  = null;
+    public $language  = 'zh-cn';
+    public $language_l = [];
     protected function initialize()
     {
-        $lang = request()->header('Accept-Language');
-        $this->language = $lang ? $lang : 'zh-cn';
+        
+        $lang_s = request()->header('Accept-Lag');
+        $lang_s = $lang_s ? $lang_s : request()->header('Accept-Language');
+        $lang_l = $this->formatAcceptLang($lang_s);
+        $this->language_l = $lang_l;
+        $this->language = $lang_l[0] ? ( $lang_l[0] == 'zh' ? 'zh-cn' : $lang_l[0]  )   : 'zh-cn' ;
+
         $this->systemConfig = $this->getSystemConfigs();
         parent::initialize();
 
     }
+
+
+    /**
+     * 返回json数据
+     * @param  integer $code    [状态码]
+     * @param  array $data    [主要数据]
+     * @param  string $message [描述]
+     * @param  array  $extra   [其它]
+     */
+  	public function formatAcceptLang($language) {
+      $lang_l = explode(',',$language);
+      $lang_format_list = [];
+      $q_array = [];
+      foreach ($lang_l as $key => $value) {
+        $temp_arr = explode(';',$value);
+        $q = isset($temp_arr[1]) ? $temp_arr[1] : 1;
+        $q_array[]  = $q;
+        $lang_format_list[$key] = ['lang'=>$temp_arr[0],'q'=>$q];
+      }
+      array_multisort($q_array, SORT_DESC,  $lang_format_list);
+      $lang = [];
+      foreach ($lang_format_list as $key => $value) {
+        $lang[] = strtolower(trim($value['lang']));
+      }
+      $lang = array_unique($lang);
+      return $lang;
+  	}
 
 
 
@@ -37,11 +70,7 @@ class Base extends Controller
      * @param  array  $extra   [其它]
      */
   	public function jsonReturn($code, $data, $message = '',$extra = array()) {
-  		header('Access-Control-Allow-Origin: *');
-  		header('Access-Control-Allow-Headers:*');
-  		if($_SERVER['REQUEST_METHOD']=='OPTIONS'){
-  			exit;
-  		}
+
       if(is_string($data)){
         $message = $data;
         $data = [];
