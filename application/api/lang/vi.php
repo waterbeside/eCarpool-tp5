d0f91ee9afe7ac4dde3d7ca2f6f8e85eef9d4ceb
waterbeside <waterbeside@163.com>
2018-10-23 10:53:13 +0800

    other: 添加越南文翻译

---
+++ application/api/lang/vi.php
@@ -0,0 +1,35 @@
+<?php
+return [
+  //通用
+  'Fail' => 'Thất bại',
+  "Can not be empty" => 'không thể để trống',
+  "Illegal access" => 'Cấm truy cập bất hợp pháp',
+  "No data"=>"Chưa có dữ liệu",
+  //用户相关
+  'Please enter user name'  => 'Vui lòng nhập tên người dùng',
+  'Please enter your password' => 'Vui lòng nhập mật khẩu của bạn',
+  'User name or password error' => 'Tên người dùng hoặc mật khẩu không chính xác',
+  'The user is banned' => 'Người dùng bị cấm',
+
+  'Please enter the correct old password' => 'Vui lòng nhập đúng mật khẩu cũ',
+  'Two passwords are different'=>'Hai mật khẩu khác nhau,vui lòng nhập lại',
+  'The new password should be no less than 6 characters'=>'Mật khẩu mới không được nhỏ hơn 6 ký tự',
+
+  'User does not exist'=>'Người dùng không tồn tại',
+  'User already exists'=>'Người dùng đã tồn tại',
+  'Already bound, please enter a new phone number' => 'Đã bị ràng buộc, vui lòng nhập số điện thoại mới',
+  'The mobile phone number has been marked with a new account, whether to merge?' => 'Số điện thoại di động đã được đánh dấu bằng một tài khoản mới, nó có được hợp nhất không?',
+  'No need to merge' => 'Không cần phải hợp nhất',
+  'The phone number has been bound to this account, no need to merge.' => 'Số điện thoại đã bị ràng buộc vào tài khoản này, không cần phải hợp nhất',
+  'The phone number has been registered for another account'=>'Số điện thoại đã được đăng ký một tài khoản khác',
+
+  //附件相关
+  "Wrong format"=>'Định dạng không chính xác',
+  "Upload successful" => 'Tải lên thành công',
+  "Attachment information failed to be written" => 'thông tin tập tin đính kèm không được viết',
+  "Please upload attachments" => 'Vui lòng tải lên tệp đính kèm',
+  "Not image file format" => 'Định dạng hình ảnh không chính xác',
+  'Images cannot be larger than 800K' => 'Hình ảnh không được lớn hơn 800KB',
+  "File not found"=>'Không tìm thấy tệp',
+  "This attachment cannot be deleted" => 'Không thể xóa tệp đính kèm này',
+];
