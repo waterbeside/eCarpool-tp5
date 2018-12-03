2013cd37250280bfc601a16027b96a14ccea8dca
waterbeside <waterbeside@163.com>
2018-12-03 10:41:48 +0800

    opt: 先查找有没有一样的地址再创建

--- application/carpool/model/Address.php
+++ application/carpool/model/Address.php
@@ -25,20 +25,38 @@ class Address extends Model
        $this->errorMsg = lang("Can not be empty");
        return false;
      }
-     //如果id为空，通过经纬度查找id.无则创建一个并返回id;
-     $data['longtitude'] = $data['longitude'];
-     $data['city'] = isset($data['city']) && $data['city'] ? $data['city'] : "-" ;
-     $data['address_type'] = 1;
-     $createID = $this->insertGetId($startDatas);
+     //先查找有没有对应的地址
+     $findMap = [
+       'addressname'=>$data['addressname'],
+       'latitude'=>$data['latitude'],
+       'longtitude'=>$data['longitude'],
+     ];
+     $res = $this
+     ->field("address_type,addressname,longtitude as longitude,latitude,create_time,company_id,city")
+     ->where($findMap)->find();
+
+     if($res){
+       $data = array_merge($data,$res->toArray());
+       return $data;
+     }
+     //如果没有数据，则创建
+     $inputData = [
+       'address_type' => 1,
+       'addressname' => $data['addressname'],
+       'longtitude' => $data['longitude'],
+       'latitude'   => $data['latitude'],
+       'create_time'   => date("Y-m-d H:i:s"),
+       'company_id'   => $data['company_id'],
+       'city'       => isset($data['city']) && $data['city'] ? $data['city'] : "--",
+     ];
+     $createID = $this->insertGetId($inputData);
      if($createID){
-       $createAddress[0] = $startDatas;
-       $createAddress[0]['addressid'] = $createID;
-       $datas['addressid'] = $createID;
+       $data['addressid'] = intval($createID);
+       $data = array_merge($data,$inputData);
      }else{
        $this->errorMsg = lang("Fail");
        return false;
      }
-
      unset($data['longtitude']);
      return $data;
    }
