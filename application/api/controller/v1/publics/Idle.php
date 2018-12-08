097f5a4d21abc0b5d512ff5baf025aed896b0612
waterbeside <waterbeside@163.com>
2018-12-08 12:29:54 +0800

    feat: 详情接口及列表筛选

--- application/api/controller/v1/publics/Idle.php
+++ application/api/controller/v1/publics/Idle.php
@@ -23,19 +23,56 @@ class Idle extends ApiBase
     }
 
 
-    public function index($pagesize=20){
-      $fields = "t.id,t.user_id,t.title,t.desc,t.images,t.location,t.price,t.status,t.is_seller,u.name,u.phone,u.loginname";
+    public function index($pagesize=24){
+      $filter['keyword']      = trim(input('param.keyword'));
+      $filter['is_seller']    = input('param.is_seller',-1);
+      $filter['start_price']  = input('param.start_price');
+      $filter['end_price']  = input('param.end_price');
+      $filter['start_time']  = input('param.start_time');
+      $filter['end_time']  = input('param.end_time');
+
+      $fields = "t.id,t.user_id,t.title,t.desc,t.images,t.location,t.price,t.status,t.is_seller,t.post_time,u.name,u.phone,u.loginname";
       $map = [
         ["is_delete","<>",1],
+        ["u.loginname","exp",Db::raw("IS NOT NULL")],
         // ["show_level",">",0],
       ];
+
+      if($filter['keyword']){
+        $map[] = ['title|desc','like','%'.$filter['keyword'].'%'];
+      }
+      if(is_numeric($filter['is_seller']) && $filter['is_seller'] > -1 ){
+        $map[] = ['is_seller','=',$filter['is_seller']];
+      }
+      if(is_numeric($filter['start_price']) ){
+        $map[] = ['price','>=',$filter['start_price']];
+      }
+      if(is_numeric($filter['end_price']) ){
+        $map[] = ['price','<=',$filter['end_price']];
+      }
+      if(is_numeric($filter['start_time']) && $filter['start_time'] > 1200000000 ){
+        $map[] = ['post_time','>= time',date('Y-m-d H:i:s',$filter['start_time'])];
+      }
+      if(is_numeric($filter['end_time']) && $filter['end_time'] > 1200000000 ){
+        $map[] = ['post_time','<= time',date('Y-m-d H:i:s',$filter['end_time'])];
+      }
       $join = [
         ['user u','u.uid = t.user_id', 'left']
       ];
       $orderby = "t.status DESC, post_time DESC";
-      $results = IdleModel::alias('t')->field($fields)->json(['t.images'])->where($map)->join($join)->order($orderby)->paginate($pagesize, false,  ['query'=>request()->param()])->toArray();
+      $results = IdleModel::alias('t')->field($fields)->json(['images'])->where($map)->join($join)->order($orderby)->paginate($pagesize, false,  ['query'=>request()->param()])->toArray();
 
       $datas = $results['data'];
+      if(!$datas){
+        $this->jsonReturn(20002,[],'No Data');
+      }
+      // dump($datas);exit;
+      foreach ($datas as $key => $value) {
+        // $datas[$key]['post_time']   =
+        $datas[$key]['thumb']       = isset($value['images'][0]->path) ? $value['images'][0]->path : '';
+        $datas[$key]['thumb']       = str_replace('http:/g','http://g',$datas[$key]['thumb']);
+        unset($datas[$key]['images']);
+      }
       $pageData = [
         'total'=>$results['total'],
         'pageSize'=>$results['per_page'],
@@ -45,10 +82,38 @@ class Idle extends ApiBase
       $returnData = [
         'lists'=>$datas,
         'page' =>$pageData,
+        'filter'=>$filter
       ];
       $this->jsonReturn(0,$returnData,'success');
     }
 
+
+    public function read($id){
+      $fields = "t.id,t.user_id,t.title,t.desc,t.images,t.location,t.price,t.status,t.is_seller,t.post_time,t.is_delete,u.name,u.phone,u.loginname";
+      $map = [
+        ["t.id","=",$id],
+      ];
+      $join = [
+        ['user u','u.uid = t.user_id', 'left']
+      ];
+      $orderby = "t.status DESC, post_time DESC";
+      $datas = IdleModel::alias('t')->field($fields)->json(['images'])->where($map)->join($join)->find();
+      if(!$datas || $datas['is_delete'] == 1){
+        $this->jsonReturn(20002,[],'No Data');
+      }
+      $datas['images'] = json_decode(json_encode($datas['images']),true);
+      $imagesList = [];
+      foreach ($datas['images'] as $key => $value) {
+        if(isset($value['path'])){
+          $value['path'] = str_replace('http:/g','http://g',$value['path']);
+        }
+        $imagesList[] = $value;
+      }
+      $datas['images'] = $imagesList;
+      $datas['thumb']       = isset($datas['images'][0]['path']) ? $datas['images'][0]['path'] : '';
+      unset($datas['is_delete']);
+      $this->jsonReturn(0,$datas,'success');
+    }
 
 
 
