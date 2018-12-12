40ee2d8c51675dbc9597dc973b1863996915f2f2
waterbeside <waterbeside@163.com>
2018-12-12 15:44:10 +0800

    opt: 加入分类筛选

--- application/api/controller/v1/publics/Idle.php
+++ application/api/controller/v1/publics/Idle.php
@@ -22,7 +22,10 @@ class Idle extends ApiBase
         // $this->checkPassport(1);
     }
 
-
+    /**
+     * 列表
+     * @param  integer $pagesize  每页条数
+     */
     public function index($pagesize=24){
       $filter['keyword']      = trim(input('param.keyword'));
       $filter['is_seller']    = input('param.is_seller',-1);
@@ -30,34 +33,43 @@ class Idle extends ApiBase
       $filter['end_price']  = input('param.end_price');
       $filter['start_time']  = input('param.start_time');
       $filter['end_time']  = input('param.end_time');
+      $filter['cateid']  = input('param.cateid');
 
       $fields = "t.id,t.user_id,t.title,t.desc,t.images,t.location,t.price,t.status,t.is_seller,t.post_time,u.name,u.phone,u.loginname";
       $map = [
-        ["is_delete","<>",1],
+        ["t.is_delete","<>",1],
         ["u.loginname","exp",Db::raw("IS NOT NULL")],
         // ["show_level",">",0],
       ];
 
       if($filter['keyword']){
-        $map[] = ['title|desc','like','%'.$filter['keyword'].'%'];
+        $map[] = ['t.title|t.desc','like','%'.$filter['keyword'].'%'];
+      }
+      if(is_numeric($filter['cateid']) && $filter['cateid'] > 0 ){
+        $map[] = ['ic.category_id','=',$filter['cateid']];
+      }
+      if(!is_numeric($filter['cateid']) && strpos($filter['cateid'],'!')===0  ){
+        $cateArr = explode('!',$filter['cateid']);
+        if(is_numeric($cateArr[1]) && $cateArr[1] >0 ) $map[] = ['ic.category_id','<>',$cateArr[1]];
       }
       if(is_numeric($filter['is_seller']) && $filter['is_seller'] > -1 ){
-        $map[] = ['is_seller','=',$filter['is_seller']];
+        $map[] = ['t.is_seller','=',$filter['is_seller']];
       }
       if(is_numeric($filter['start_price']) ){
-        $map[] = ['price','>=',$filter['start_price']];
+        $map[] = ['t.price','>=',$filter['start_price']];
       }
       if(is_numeric($filter['end_price']) ){
-        $map[] = ['price','<=',$filter['end_price']];
+        $map[] = ['t.price','<=',$filter['end_price']];
       }
       if(is_numeric($filter['start_time']) && $filter['start_time'] > 1200000000 ){
-        $map[] = ['post_time','>= time',date('Y-m-d H:i:s',$filter['start_time'])];
+        $map[] = ['t.post_time','>= time',date('Y-m-d H:i:s',$filter['start_time'])];
       }
       if(is_numeric($filter['end_time']) && $filter['end_time'] > 1200000000 ){
-        $map[] = ['post_time','<= time',date('Y-m-d H:i:s',$filter['end_time'])];
+        $map[] = ['t.post_time','<= time',date('Y-m-d H:i:s',$filter['end_time'])];
       }
       $join = [
-        ['user u','u.uid = t.user_id', 'left']
+        ['user u','u.uid = t.user_id', 'left'],
+        ['(select max(category_id) as category_id ,idle_id from t_idle_category group by idle_id) ic','ic.idle_id = t.id', 'left']
       ];
       $orderby = "t.status DESC, post_time DESC";
       $results = IdleModel::alias('t')->field($fields)->json(['images'])->where($map)->join($join)->order($orderby)->paginate($pagesize, false,  ['query'=>request()->param()])->toArray();
@@ -77,7 +89,7 @@ class Idle extends ApiBase
         'total'=>$results['total'],
         'pageSize'=>$results['per_page'],
         'lastPage'=>$results['last_page'],
-        'currentPage'=>$results['current_page'],
+        'currentPage'=> intval($results['current_page']),
       ];
       $returnData = [
         'lists'=>$datas,
@@ -88,6 +100,11 @@ class Idle extends ApiBase
     }
 
 
+
+    /**
+     * 详情
+     * @param  integer id  id
+     */
     public function read($id){
       $fields = "t.id,t.user_id,t.title,t.desc,t.images,t.location,t.price,t.status,t.is_seller,t.post_time,t.is_delete,u.name,u.phone,u.loginname";
       $map = [
