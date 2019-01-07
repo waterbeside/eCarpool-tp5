7c52c766d6747f38838b5da7d991fc5bc7c81093
waterbeside <waterbeside@163.com>
2019-01-07 14:00:21 +0800

    feat: 接用户信息

--- application/api/controller/v1/User.php
+++ application/api/controller/v1/User.php
@@ -3,7 +3,8 @@ namespace app\api\controller\v1;
 
 use app\api\controller\ApiBase;
 use app\carpool\model\UserPosition;
-use app\carpool\model\UserModel;
+use app\carpool\model\User as UserModel;
+use app\user\model\Department as DepartmentModel;
 
 use think\Db;
 
@@ -19,6 +20,26 @@ class User extends ApiBase
     {
         parent::initialize();
         // $this->checkPassport(1);
+    }
+
+
+    /**
+     *
+     * @param  int  $id
+     */
+    public function read($id)
+    {
+      $this->checkPassport(1);
+      $field = 't.uid, t.name , t.loginname, t.phone, t.mobile, t.sex,  t.Department as department, d.fullname as full_department';
+      $map   = [];
+      $map[] = ['is_delete','<>',1];
+      $map[] = ['uid','=',$id];
+      $join[] = ['t_department d','d.id = t.department_id', 'left'];
+      $data  = UserModel::field($field)->alias('t')->join($join)->where($map)->find();
+      if(!$data){
+        return $this->jsonReturn(20002,$data);
+      }
+      return $this->jsonReturn(0,$data);
     }
 
 
