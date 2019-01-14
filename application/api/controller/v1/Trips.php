a32205134f592f873c0c29dfadd019017b69c963
waterbeside <waterbeside@163.com>
2019-01-14 14:19:32 +0800

    other: 加上返回汽车颜色

--- application/api/controller/v1/Trips.php
+++ application/api/controller/v1/Trips.php
@@ -890,7 +890,7 @@ class Trips extends ApiBase
      */
     protected function buildUserFields($a="u",$fields=[]){
       $format_array = [];
-      $fields = !empty($fields) ? $fields : ['uid','loginname','name','phone','mobile','Department','sex','company_id','department_id','companyname','imgpath','carnumber','im_id'];
+      $fields = !empty($fields) ? $fields : ['uid','loginname','name','phone','mobile','Department','sex','company_id','department_id','companyname','imgpath','carnumber','carcolor','im_id'];
 
       foreach ($fields as $key => $value) {
         $format_array[$key] = $a.".".$value." as ".$a."_".mb_strtolower($value);
