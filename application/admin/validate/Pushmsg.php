839d7601ca5c91aa9486903d23219ea9841805e0
waterbeside <waterbeside@163.com>
2019-02-21 14:26:43 +0800

    feat: 给指定用户发送推送

---
+++ application/admin/validate/Pushmsg.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\admin\validate;
+
+use think\Validate;
+
+/**
+ * 推送消息验证
+ * Class Slide
+ * @package app\admin\validate
+ */
+class Pushmsg extends Validate
+{
+    protected $rule = [
+        'body'  => 'require',
+        'title' => 'require',
+    ];
+
+    protected $message = [
+        'title.require'  => '请填写标题',
+        'body.require' => '请填写内容',
+    ];
+}
