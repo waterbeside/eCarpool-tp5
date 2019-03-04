00bb8823b3de302ff08c0424d0a58ac448aaa24e
waterbeside <waterbeside@163.com>
2019-03-04 10:47:57 +0800

    other:  beautify code and add  comments,

--- application/admin/controller/SyncHr.php
+++ application/admin/controller/SyncHr.php
@@ -1,7 +1,6 @@
 <?php
 namespace app\admin\controller;
 
-
 use app\admin\controller\AdminBase;
 
 use app\carpool\model\User as OldUserModel;
@@ -13,7 +12,7 @@ use think\Db;
 
 /**
  * 同步hr系统
- * Class Passport
+ * Class SyncHr
  * @package app\api\controller
  */
 class SyncHr extends AdminBase
@@ -22,136 +21,139 @@ class SyncHr extends AdminBase
 
 
   /**
-   * 用户管理
-   * @param string $keyword
-   * @param int    $page
+   * 同步用户数据列表
+   * @param array $filter 列表过滤参数
+   * @param int    $page 页码
+   * @param int    $pagesize 每页条数
    * @return mixed
    */
-  public function index($filter = [], $page = 1,$pagesize = 50)
-  {
-      $map = [];
-      //筛选用户信息
-      if (isset($filter['keyword']) && $filter['keyword'] ){
-        $map[] = ['code|name','like', "%{$filter['keyword']}%"];
-      }
-      //筛选部门
-      if (isset($filter['keyword_dept']) && $filter['keyword_dept'] ){
-        $map[] = ['department','like', "%{$filter['keyword_dept']}%"];
-      }
-      //筛选部门
-      if (isset($filter['status']) &&  is_numeric($filter['status'])  ){
-        $map[] = ['status','=', $filter['status']];
-      }
-      $UserTemp = new UserTemp();
-      $lastTime = $UserTemp->getLastExecuteTime();
-      $lists = $UserTemp->alias('u')->where($map)->order('id DESC')->paginate($pagesize, false,  ['query'=>request()->param()]);
+    public function index($filter = [], $page = 1, $pagesize = 50)
+    {
+        $map = [];
+        //筛选用户信息
+        if (isset($filter['keyword']) && $filter['keyword']) {
+            $map[] = ['code|name','like', "%{$filter['keyword']}%"];
+        }
+        //筛选部门
+        if (isset($filter['keyword_dept']) && $filter['keyword_dept']) {
+            $map[] = ['department','like', "%{$filter['keyword_dept']}%"];
+        }
+        //筛选部门
+        if (isset($filter['status']) &&  is_numeric($filter['status'])) {
+            $map[] = ['status','=', $filter['status']];
+        }
+        $UserTemp = new UserTemp();
+        $lastTime = $UserTemp->getLastExecuteTime();
+        $lists = $UserTemp->alias('u')->where($map)->order('id DESC')->paginate($pagesize, false, ['query'=>request()->param()]);
 
-      // dump($lists);exit;
-      //
-      $port = $this->request->port();
-      return $this->fetch('index', ['lastTime'=>$lastTime,'lists' => $lists, 'filter' => $filter,'pagesize'=>$pagesize,'port'=>$port]);
-  }
-
-
-
-  /**
-   *
-   */
-
-  public function sync_all($date = null, $type = 0, $page = 0, $pagesize = 10,$return = 1 )
-  {
-
-    ini_set ('memory_limit', '128M');
-    ini_set('max_execution_time','180');
-    $url = "http://127.0.0.1:8082/api/v1/sync_hr/all";
-    $params = [
-      'type'=>$type,
-      'page'=>$page,
-      'pagesize'=>$pagesize,
-    ];
-    if($date){ $params['date'] = $date; }
-
-    try {
-      $client = new \GuzzleHttp\Client(['verify'=>false]);
-      // $client->setDefaultOption('verify', false);
-      $response = $client->request('get', $url, ['query' => $params]);
-      $content = $response->getBody()->getContents();
-
-      $res = json_decode($content,true);
-    } catch (Exception $e) {
-      // $this->errorMsg ='拉取失败';
-      $this->error("同步失败");
-    }
-    if(!$res){
-      $this->error("同步失败");
-    }
-    if($res && $res['code'] ===20002){
-      return  $type==1 && $page >0 && $return ? $this->fetch('index/multi_jump',['url'=>"",'msg'=>$res['desc']]) : $this->jsonReturn($res['code'],$res['data'],$res['desc']);
-    }
-    if($res && $res['code'] !== 0){
-      return $this->jsonReturn($res['code'],$res['data'],$res['desc']);
+        // dump($lists);exit;
+        //
+        $port = $this->request->port();
+        return $this->fetch('index', ['lastTime'=>$lastTime,'lists' => $lists, 'filter' => $filter,'pagesize'=>$pagesize,'port'=>$port]);
     }
 
-    $data = $res['data'];
-    if($type == 1 && $page > 0  && $return ){
-          $jumpUrl  = url('sync_all',['date'=>$date,'type'=>$type,'page'=>$page+1,'pagesize'=>$pagesize,'return'=>$return]);
-          if($data['total'] > 0){
-            $msg = "total:".$data['total']."<br />";
-            $successMsg = "success:<br />";
-            foreach ( explode(',',$data['success']) as $key => $value) {
-              $br = $key%2 == 1 ? "<br />" : "";
-              $successMsg .= $value.",".$br;
+
+
+    /**
+     * 同步所有用户
+     * @param  string  $date     拉取数据的起始日
+     * @param  integer $type
+     *  $type = 0 仅执行从Hr接口拉到本地临时表（t_user_temp）
+     *  $type = 1 仅执行从本地临时表同步到正式表
+     *  $type = 2 执行 type = 0后，并执行1. （拉取到临时库，并同步到正式库。数据量大时不建议使用。）
+     * @param  integer $page     页码 当type ==1 时有效。设为page > 0时，分页从t_user_temp同步到正式表
+     * @param  integer $pagesize 当设有page参数时，设置每页执行多少条数据。
+     */
+    public function sync_all($date = null, $type = 0, $page = 0, $pagesize = 10, $return = 1)
+    {
+        ini_set('memory_limit', '128M');
+        ini_set('max_execution_time', '180');
+        $url = "http://127.0.0.1:8082/api/v1/sync_hr/all";
+        $params = [
+          'type'=>$type,
+          'page'=>$page,
+          'pagesize'=>$pagesize,
+        ];
+        if ($date) {
+            $params['date'] = $date;
+        }
+
+        try {
+            $client = new \GuzzleHttp\Client(['verify'=>false]);
+            // $client->setDefaultOption('verify', false);
+            $response = $client->request('get', $url, ['query' => $params]);
+            $content = $response->getBody()->getContents();
+
+            $res = json_decode($content, true);
+        } catch (Exception $e) {
+            // $this->errorMsg ='拉取失败';
+            $this->error("同步失败");
+        }
+        if (!$res) {
+            $this->error("同步失败");
+        }
+        if ($res && $res['code'] ===20002) {
+            return  $type==1 && $page >0 && $return ? $this->fetch('index/multi_jump', ['url'=>"",'msg'=>$res['desc']]) : $this->jsonReturn($res['code'], $res['data'], $res['desc']);
+        }
+        if ($res && $res['code'] !== 0) {
+            return $this->jsonReturn($res['code'], $res['data'], $res['desc']);
+        }
+
+        $data = $res['data'];
+        if ($type == 1 && $page > 0  && $return) {
+            $jumpUrl  = url('sync_all', ['date'=>$date,'type'=>$type,'page'=>$page+1,'pagesize'=>$pagesize,'return'=>$return]);
+            if ($data['total'] > 0) {
+                $msg = "total:".$data['total']."<br />";
+                $successMsg = "success:<br />";
+                foreach (explode(',', $data['success']) as $key => $value) {
+                    $br = $key%2 == 1 ? "<br />" : "";
+                    $successMsg .= $value.",".$br;
+                }
+                $failMsg = "fail:<br />";
+                foreach (explode(',', $data['fail']) as $key => $value) {
+                    $br = $key%2 == 1 ? "<br />" : "";
+                    $failMsg .= $value.",".$br;
+                }
+                $msg .= $successMsg."<br />".$failMsg."<br />";
+                return $this->fetch('index/multi_jump', ['url'=>$jumpUrl,'msg'=>$msg]);
+            } else {
+                return $this->success("同步完成");
             }
-            $failMsg = "fail:<br />";
-            foreach ( explode(',',$data['fail']) as $key => $value) {
-              $br = $key%2 == 1 ? "<br />" : "";
-              $failMsg .= $value.",".$br;
-            }
-            $msg .= $successMsg."<br />".$failMsg."<br />";
-            return $this->fetch('index/multi_jump',['url'=>$jumpUrl,'msg'=>$msg]);
-          }else{
-            return $this->success("同步完成");
-          }
-    }else{
-      return $this->jsonReturn($res['code'],$res['data'],$res['desc']);
+        } else {
+            return $this->jsonReturn($res['code'], $res['data'], $res['desc']);
+        }
     }
 
-  }
 
 
 
-
-
-    public function sync_single($code=0,$tid=0){
-
-      $url = "http://127.0.0.1:8082/api/v1/sync_hr/single";
-      $params = [
+    /**
+     * 同步单用户接口
+     * @param  integer $code    员工号，使用该参数后，会先从HR拉取信息到临时表t_user_temp并马上执行同步到正式表
+     * @param  integer $tid      t_user_temp表的行id，使用该参数后，直接从t_user_temp表执行同步到正式表。当使用参数code时，tid参数无效。
+     */
+    public function sync_single($code=0, $tid=0)
+    {
+        $url = "http://127.0.0.1:8082/api/v1/sync_hr/single";
+        $params = [
         'code'=>$code,
         'tid'=>$tid,
         'is_sync'=>1,
       ];
 
-      try {
-        $client = new \GuzzleHttp\Client(['verify'=>false]);
-        // $client->setDefaultOption('verify', false);
-        $response = $client->request('get', $url, ['query' => $params]);
-        $content = $response->getBody()->getContents();
-        $res = json_decode($content,true);
-      } catch (\GuzzleHttp\Exception\ClientException $exception) {
-        $responseBody = $exception->getResponse()->getBody()->getContents();
-        $this->jsonReturn(0,'同步失败');
-      }
-      if($content){
-        return $this->jsonReturn($res['code'],$res['data'],$res['desc']);
-      }
-      exit;
+        try {
+            $client = new \GuzzleHttp\Client(['verify'=>false]);
+            // $client->setDefaultOption('verify', false);
+            $response = $client->request('get', $url, ['query' => $params]);
+            $content = $response->getBody()->getContents();
+            $res = json_decode($content, true);
+        } catch (\GuzzleHttp\Exception\ClientException $exception) {
+            $responseBody = $exception->getResponse()->getBody()->getContents();
+            $this->jsonReturn(0, '同步失败');
+        }
+        if ($content) {
+            return $this->jsonReturn($res['code'], $res['data'], $res['desc']);
+        }
+        exit;
     }
-
-
-
-
-
-
-
-
 }
--- application/api/controller/v1/SyncHr.php
+++ application/api/controller/v1/SyncHr.php
@@ -17,149 +17,167 @@ use think\Db;
  */
 class SyncHr extends ApiBase
 {
-
     protected function initialize()
     {
         parent::initialize();
     }
 
 
-    protected function check_localhost($returnType = 0){
-      $host = $this->request->host();
-      // dump($this->request->port());exit;
-      // && !in_array($host,["gitsite.net:8082","admin.carpoolchina.test"])
-      if(strpos($host,'127.0.0.1') === false && strpos($host,'localhost') === false ){
-        return $returnType ?  $this->error(lang('Illegal access')) : false ;
-      }else{
-        return true;
-      }
+    /**
+     * 验证是否本地请求
+     * @param  integer $returnType 0返回true or false，其它当false时，返json
+     * @return boolean
+     */
+    protected function check_localhost($returnType = 0)
+    {
+        $host = $this->request->host();
+        // dump($this->request->port());exit;
+        // && !in_array($host,["gitsite.net:8082","admin.carpoolchina.test"])
+        if (strpos($host, '127.0.0.1') === false && strpos($host, 'localhost') === false) {
+            return $returnType ?  $this->error(lang('Illegal access')) : false ;
+        } else {
+            return true;
+        }
     }
 
 
     /**
-     *
+     * 同步全部
+     * @param  string  $date     拉取数据的起始日
+     * @param  integer $type
+     *  $type = 0 仅执行从Hr接口拉到本地临时表（t_user_temp）
+     *  $type = 1 仅执行从本地临时表同步到正式表
+     *  $type = 2 执行 type = 0后，并执行1. （拉取到临时库，并同步到正式库。数据量大时不建议使用。）
+     * @param  integer $page     页码 当type ==1 时有效。设为page > 0时，分页从t_user_temp同步到正式表
+     * @param  integer $pagesize 当设有page参数时，设置每页执行多少条数据。
      */
-    public function all($date = null, $type = 0, $page = 0, $pagesize = 30 )
+    public function all($date = null, $type = 0, $page = 0, $pagesize = 30)
     {
-      $this->check_localhost(1);
-      ini_set ('memory_limit', '128M');
-      ini_set('max_execution_time','180');
+        $this->check_localhost(1);
+        ini_set('memory_limit', '128M');
+        ini_set('max_execution_time', '180');
 
-      $userTempModel = new UserTemp();
-      if($type == 1){
-        $whereMap = [
-          ['status','<',1],
-          ['status','>',-2],
-        ];
-        if($date){
-          $whereMap[] = ['modifty_time', '>=' ,$date];
+        $userTempModel = new UserTemp();
+        if ($type == 1) {
+            $whereMap = [
+              ['status','<',1],
+              ['status','>',-2],
+            ];
+            if ($date) {
+                $whereMap[] = ['modifty_time', '>=' ,$date];
+            }
+            if ($page > 0) {
+                // $res =  $userTempModel->where($whereMap)->page($page,$pagesize)->select();
+                $total =  $userTempModel->where($whereMap)->count();
+                if ($total < 1) {
+                    $this->jsonReturn(20002, ($page > 1 ? '同步完成' : '没有要更新的数据'));
+                }
+                $res =  $userTempModel->where($whereMap)->limit($pagesize)->select();
+            } else {
+                $res =  $userTempModel->where($whereMap)->select();
+            }
+            if (!$res) {
+                $this->jsonReturn(20002, "没有要更新的数据");
+            }
+        } else {
+            $res = $userTempModel->pullListFromHr($date);
+            if ($res===false) {
+                $this->jsonReturn(-1, $userTempModel->errorMsg);
+            }
         }
-        if($page > 0 ){
-          // $res =  $userTempModel->where($whereMap)->page($page,$pagesize)->select();
-          $total =  $userTempModel->where($whereMap)->count();
-          if($total < 1){
-            $this->jsonReturn(20002,($page > 1 ? '同步完成' : '没有要更新的数据'));
-          }
-          $res =  $userTempModel->where($whereMap)->limit($pagesize)->select();
-        }else{
-          $res =  $userTempModel->where($whereMap)->select();
-        }
-        if(!$res){
-          $this->jsonReturn(20002,"没有要更新的数据");
-        }
-      }else{
-        $res = $userTempModel->pullListFromHr($date);
-        if($res===false){
-          $this->jsonReturn(-1,$userTempModel->errorMsg);
-        }
-      }
 
-      $success ='';
-      $fail = '';
-      if($type > 0){
-        foreach ($res as $key => $value) {
-          $res_toPrimary = $userTempModel->toPrimary($value['code'],$value);
-          if($res_toPrimary){
-            $success .=$value['code'].",";
-          }else{
-            $fail    .=$value['code'].",";
-          }
+        $success ='';
+        $fail = '';
+        if ($type > 0) {
+            foreach ($res as $key => $value) {
+                $res_toPrimary = $userTempModel->toPrimary($value['code'], $value);
+                if ($res_toPrimary) {
+                    $success .=$value['code'].",";
+                } else {
+                    $fail    .=$value['code'].",";
+                }
+            }
         }
-      }
-      if($type == 1 && $page > 0 ){
-          $returnData = [
+        if ($type == 1 && $page > 0) {
+            $returnData = [
             "list"=>$res,
             "total"=>$total,
             "page"=>$page,
             "success"=>$success,
             "fail"=>$fail,
           ];
-          $this->jsonReturn(0,$returnData,"同步成功");
-      }else{
-          $returnData = [
+            $this->jsonReturn(0, $returnData, "同步成功");
+        } else {
+            $returnData = [
             "list"=>$res,
           ];
-          if($type > 0){
-            $returnData['success']=$success;
-            $returnData['fail']=$fail;
-            $returnData['total']=count($res);
-          }
-          $this->jsonReturn(0,$returnData,"拉取成功");
-      }
+            if ($type > 0) {
+                $returnData['success']=$success;
+                $returnData['fail']=$fail;
+                $returnData['total']=count($res);
+            }
+            $this->jsonReturn(0, $returnData, "拉取成功");
+        }
     }
 
 
+    /**
+     * 同步单用户接口
+     * @param  integer $code    员工号，使用该参数后，会先从HR拉取信息到临时表t_user_temp并马上执行同步到正式表
+     * @param  integer $tid      t_user_temp表的行id，使用该参数后，直接从t_user_temp表执行同步到正式表。当使用参数code时，tid参数无效。
+     * @param  integer $is_sync 当为1时，执行同步 ，默认为0，不执行同步，只查询。
+     */
+    public function single($code=0, $tid=0, $is_sync=0)
+    {
+        if (!$this->check_localhost() && !$this->checkPassport()) {
+            $this->error(lang('Illegal access'));
+        }
+        if (!$code && !$tid) {
+            return $this->jsonReturn(992, [], lang('Parameter error'));
+        }
+        $userTempModel = new UserTemp();
+        if ($code) {
+            $tid = 0;
+            $res = $userTempModel->pullUserFromHr($code, $is_sync);
+            if (!$res) {
+                $this->jsonReturn(-1, $userTempModel->errorMsg);
+            }
+        }
+        if ($tid) {
+            $res = $userTempModel->where('id', $tid)->find();
+            if (!$res) {
+                return $this->jsonReturn(20002, '无此数据');
+            }
+            if (!in_array($res['status'], [-1,0])) {
+                return $this->jsonReturn(-1, ['status'=>$res['status']], '已同步过，无需再同步');
+            }
+        }
 
-    public function single($code=0,$tid=0,$is_sync=0){
-      if(!$this->check_localhost() && !$this->checkPassport()){
-        $this->error(lang('Illegal access'));
-      }
-      if(!$code && !$tid){
-        return $this->jsonReturn(992,[],lang('Parameter error'));
-      }
-      $userTempModel = new UserTemp();
-      if($code){
-        $tid = 0;
-        $res = $userTempModel->pullUserFromHr($code,$is_sync);
-        if(!$res){
-          $this->jsonReturn(-1,$userTempModel->errorMsg);
+        if ($res['code'] == -2) {
+            return $this->jsonReturn(-1, $res, $userTempModel->errorMsg);
         }
-      }
-      if($tid){
-        $res = $userTempModel->where('id',$tid)->find();
-        if(!$res){
-          return $this->jsonReturn(20002,'无此数据');
-        }
-        if(!in_array($res['status'],[-1,0])){
-          return $this->jsonReturn(-1,['status'=>$res['status']],'已同步过，无需再同步');
-        }
-      }
 
-      if($res['code'] == -2){
-        return $this->jsonReturn(-1,$res,$userTempModel->errorMsg);
-      }
-
-      if($res['code'] == -1 ){
-        $userData = OldUserModel::where('loginname',$code)->find();
-        if(!$userData){
-          return $this->jsonReturn(20002,$res,"用户不存在");
+        if ($res['code'] == -1) {
+            $userData = OldUserModel::where('loginname', $code)->find();
+            if (!$userData) {
+                return $this->jsonReturn(20002, $res, "用户不存在");
+            }
+            if ($userData &&  in_array($userData["company_id"], [1,11]) && !in_array($userData['Department'], ['李宁','高明常安花园','高明一中','佛山市政府'])) {
+                // if($is_sync) OldUserModel::where('uid',$userData['uid'])->update(['is_active'=>0,'modifty_time'=>date("Y-m-d H:i:s")]);
+                return $this->jsonReturn(10003, $res, "用户已离积");
+            }
+            return $this->jsonReturn(20002, $res, "用户不存在");
         }
-        if($userData &&  in_array($userData["company_id"],[1,11]) && !in_array($userData['Department'],['李宁','高明常安花园','高明一中','佛山市政府'])){
-          // if($is_sync) OldUserModel::where('uid',$userData['uid'])->update(['is_active'=>0,'modifty_time'=>date("Y-m-d H:i:s")]);
-          return $this->jsonReturn(10003,$res,"用户已离积");
+        if ($is_sync) {
+            $res_toPrimary = $userTempModel->toPrimary($res['code'], $res);
+            if (!$res_toPrimary) {
+                $this->jsonReturn(-1, ['status'=>$res['status']], ($tid ? "同步失败": $userTempModel->errorMsg));
+            }
+            $this->jsonReturn(0, $res_toPrimary, "同步成功");
+        } else {
+            $this->jsonReturn(0, $res, "success");
         }
-        return $this->jsonReturn(20002,$res,"用户不存在");
-      }
-      if($is_sync){
-        $res_toPrimary = $userTempModel->toPrimary($res['code'],$res);
-        if(!$res_toPrimary){
-          $this->jsonReturn(-1,['status'=>$res['status']],($tid ? "同步失败": $userTempModel->errorMsg ));
-        }
-        $this->jsonReturn(0,$res_toPrimary,"同步成功");
-      }else{
-        $this->jsonReturn(0,$res,"success");
-      }
-      exit;
+        exit;
     }
 
 
@@ -169,64 +187,63 @@ class SyncHr extends ApiBase
      * @param  [type] $data [description]
      * @return [type]       [description]
      */
-    public function to_primary($code=''){
-      $this->check_localhost(1);
-      $userTempModel = new UserTemp();
-      $res = $userTempModel->toPrimary($code);
-      if(!$res){
-        $this->jsonReturn(-1,$userTempModel->errorMsg);
-      }
-     $this->jsonReturn(0,$res,$userTempModel->errorMsg);
-
+    public function to_primary($code='')
+    {
+        $this->check_localhost(1);
+        $userTempModel = new UserTemp();
+        $res = $userTempModel->toPrimary($code);
+        if (!$res) {
+            $this->jsonReturn(-1, $userTempModel->errorMsg);
+        }
+        $this->jsonReturn(0, $res, $userTempModel->errorMsg);
     }
 
 
     /**
      * 创建部门 并返回部门 id
      */
-    public function create_department(){
-       $this->check_localhost(1);
-       $fullname  = input('post.fullname');
-       if(!$fullname){
-         $this->jsonReturn(992,'Param:fullname error');
-       }
-       // $sep       = input('post.sep',',');
-       // if(!in_array($sep,['/',','])){
-       //   $this->jsonReturn(992,'Param:ep error');
-       // }
-       $fullname = str_replace(',','/',$fullname);
+    public function create_department()
+    {
+        $this->check_localhost(1);
+        $fullname  = input('post.fullname');
+        if (!$fullname) {
+            $this->jsonReturn(992, 'Param:fullname error');
+        }
+        // $sep       = input('post.sep',',');
+        // if(!in_array($sep,['/',','])){
+        //   $this->jsonReturn(992,'Param:ep error');
+        // }
+        $fullname = str_replace(',', '/', $fullname);
 
-       $DepartmentModel = new Department();
+        $DepartmentModel = new Department();
 
-       $res = $DepartmentModel->create_department_by_str($fullname);
-       if(!$res){
-         $this->jsonReturn(-1,'fail');
-       }
-       $res['format_name'] = $DepartmentModel->formatFullName($res['fullname'],1);
-       $returnData = $res;
-       $this->jsonReturn(0,$returnData,'success');
+        $res = $DepartmentModel->create_department_by_str($fullname);
+        if (!$res) {
+            $this->jsonReturn(-1, 'fail');
+        }
+        $res['format_name'] = $DepartmentModel->formatFullName($res['fullname'], 1);
+        $returnData = $res;
+        $this->jsonReturn(0, $returnData, 'success');
     }
 
 
     /**
      * 取得部门
      */
-    public function department($id,$uncache=0){
-       $this->check_localhost(1);
-       $DepartmentModel = new Department();
-       if($uncache){
-         $department = $DepartmentModel->getItem($id,0);
-       }else{
-         $department = $DepartmentModel->getItem($id);
-       }
+    public function department($id, $uncache=0)
+    {
+        $this->check_localhost(1);
+        $DepartmentModel = new Department();
+        if ($uncache) {
+            $department = $DepartmentModel->getItem($id, 0);
+        } else {
+            $department = $DepartmentModel->getItem($id);
+        }
 
-       if(!$department){
-         $this->jsonReturn(20002,Lang('No data'));
-       }
+        if (!$department) {
+            $this->jsonReturn(20002, Lang('No data'));
+        }
 
-       $this->jsonReturn(0,$department,'success');
+        $this->jsonReturn(0, $department, 'success');
     }
-
-
-
 }
