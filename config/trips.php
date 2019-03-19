856dc2554fd271165c5588250f9253e9d664180d
waterbeside <waterbeside@163.com>
2019-03-19 13:03:32 +0800

    opt: 加入评分结束时间配置

--- config/trips.php
+++ config/trips.php
@@ -2,5 +2,6 @@
 
 return [
   // 'grade_start_date' =>  "2019-03-18 00:00:00"
-    'grade_start_date' =>  "2017-03-18 00:00:00"
+    'grade_start_date' =>  "2018-03-18 00:00:00",
+    'grade_end_date' =>  "2020-03-18 00:00:00"
 ];
