20f78166c0f86c933b7bfc4408ff72fda5f56bf9
waterbeside <waterbeside@163.com>
2019-07-08 10:26:26 +0800

    opt: 改进创建用户方法

--- extend/com/Nim.php
+++ extend/com/Nim.php
@@ -279,22 +279,26 @@ class Nim {
      * 创建云信ID
      * 1.第三方帐号导入到云信平台；
      * 2.注意accid，name长度以及考虑管理秘钥token
-     * @param  $accid     [云信ID，最大长度32字节，必须保证一个APP内唯一（只允许字母、数字、半角下划线_、@、半角点以及半角-组成，不区分大小写，会统一小写处理）]
-     * @param  $name      [云信ID昵称，最大长度64字节，用来PUSH推送时显示的昵称]
-     * @param  $props     [json属性，第三方可选填，最大长度1024字节]
-     * @param  $icon      [云信ID头像URL，第三方可选填，最大长度1024]
-     * @param  $token     [云信ID可以指定登录token值，最大长度128字节，并更新，如果未指定，会自动生成token，并在创建成功后返回]
-     * @return $result    [返回array数组对象]
+     * @param  array||string $accid  当为数组时，为直接指定请求云信接口的所有参数，其它参数会效   [云信ID，最大长度32字节，必须保证一个APP内唯一（只允许字母、数字、半角下划线_、@、半角点以及半角-组成，不区分大小写，会统一小写处理）]
+     * @param  string $name      [云信ID昵称，最大长度64字节，用来PUSH推送时显示的昵称]
+     * @param  string $props     [json属性，第三方可选填，最大长度1024字节]
+     * @param  string $icon      [云信ID头像URL，第三方可选填，最大长度1024]
+     * @param  string $token     [云信ID可以指定登录token值，最大长度128字节，并更新，如果未指定，会自动生成token，并在创建成功后返回]
+     * @return string $result    [返回array数组对象]
      */
     public function createUserId($accid,$name='',$props='{}',$icon='',$token=''){
-    	$url = 'https://api.netease.im/nimserver/user/create.action';
-    	$data= array(
-    		'accid' => $accid,
-    		'name'  => $name,
-    		'props' => $props,
-    		'icon'  => $icon,
-    		'token' => $token
-    	);
+        $url = 'https://api.netease.im/nimserver/user/create.action';
+        if(is_array($accid)){
+            $data = $accid;
+        }else{
+            $data = array(
+                'accid' => $accid,
+                'name'  => $name,
+                'props' => $props,
+                'icon'  => $icon,
+                'token' => $token
+            );
+        }
     	if($this->RequestType=='curl'){
             $result = $this->postDataCurl($url,$data);
         }else{
