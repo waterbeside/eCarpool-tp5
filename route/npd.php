70392a87e3054cbe2278d818e5095e953d75f89e
waterbeside <waterbeside@163.com>
2019-07-08 10:58:38 +0800

    feat: NPD相关接口

---
+++ route/npd.php
@@ -0,0 +1,12 @@
+<?php
+
+$allowHeader = 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With,Accept-Lag,Accept-Language';
+
+Route::group([], function () {
+  Route::rule('npd/api/:version/nav','npd/api.:version.nav/index','get');
+  Route::rule('npd/api/:version/category','npd/api.:version.category/index','get');
+
+ 
+
+})->header('Access-Control-Allow-Headers', $allowHeader)->allowCrossDomain();
+
