6bf19b82d7792ad606c7c6d3385577d6022b5404
waterbeside <waterbeside@163.com>
2019-07-16 17:26:01 +0800

    feat: Npd Banner验证器

---
+++ application/npd/validate/Banner.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\npd\validate;
+
+use think\Validate;
+
+class Banner extends Validate
+{
+    protected $rule = [
+        // 'cid'   => 'require',
+        'title' => 'require',
+    ];
+
+    protected $message = [
+        // 'cid.require'   => '请选择所属分类',
+        'title.require' => '请输入标题',
+    ];
+}
