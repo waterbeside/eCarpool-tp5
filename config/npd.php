d87922a48b630e113c38822b7984e2dad344ae01
waterbeside <waterbeside@163.com>
2019-07-16 17:24:52 +0800

    feat: 添加category_model_list 设置

--- config/npd.php
+++ config/npd.php
@@ -3,4 +3,17 @@ return [
   "banner_type" => [
     '1' => '首页Banner',
   ],
+
+  "category_model_list" =>[
+    [
+      'value'=>'single',
+      'name'=>'单页模型'
+    ],[
+      'value'=>'product',
+      'name'=>'产品模型'
+    ],[
+      'value'=>'article',
+      'name'=>'文章模型'
+    ]
+  ]
 ];
