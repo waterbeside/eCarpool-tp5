e424611548a7dcf13d1a81ab03d7ae7671258938
waterbeside <waterbeside@163.com>
2019-07-29 10:38:48 +0800

    feat: 客户列表、单页文章接口路由

--- route/npd.php
+++ route/npd.php
@@ -6,9 +6,12 @@ Route::group([], function () {
   Route::rule('npd/api/:version/nav','npd/api.:version.nav/index','get');
   Route::rule('npd/api/:version/category','npd/api.:version.category/index','get');
   Route::rule('npd/api/:version/banner','npd/api.:version.banner/index','get');
+  Route::rule('npd/api/:version/customer','npd/api.:version.customer/index','get');
 
   Route::resource('npd/api/:version/product','npd/api.:version.product');
   Route::resource('npd/api/:version/article','npd/api.:version.article');
+  Route::rule('npd/api/:version/single','npd/api.:version.single/index','get');
+  Route::rule('npd/api/:version/single/:id','npd/api.:version.single/read','get');
  
 
 })->header('Access-Control-Allow-Headers', $allowHeader)->allowCrossDomain();
