d476011d1aad73b48734dc266f496f1f3bbcd6cc
waterbeside <waterbeside@163.com>
2019-08-06 10:48:19 +0800

    update

--- application/score/model/AccountMix.php
+++ application/score/model/AccountMix.php
@@ -61,9 +61,9 @@ class AccountMix extends AccountModel
       $accountInfo = NULL;
       if( $type=='0' || $type=="score" ){ //直接从积分帐号取
         if($account_id){
-          $accountInfo = $this->where([['id','=',$account_id],['is_delete','<>', 1]])->find();
+          $accountInfo = $this->where([['id','=',$account_id],['is_delete','=', 0]])->find();
         }else{
-          $accountInfo = $this->where([['account','=',$account],['is_delete','<>', 1]])->find();
+          $accountInfo = $this->where([['account','=',$account],['is_delete','=', 0]])->find();
         }
         if(!$accountInfo){
           return false;
@@ -72,7 +72,7 @@ class AccountMix extends AccountModel
           $accountInfo['carpool'] = $this->getCarpoolAccount($accountInfo['carpool_account']);
         }
       }else if($type=='2'||$type=="carpool"){ //从拼车帐号取
-        $accountInfo = $this->where([['carpool_account','=',$account],['is_delete','<>', 1]])->find();
+        $accountInfo = $this->where([['carpool_account','=',$account],['is_delete','=', 0]])->find();
         $accountInfo = $accountInfo ? $accountInfo->toArray() :[];
         if($returnAll){
           $accountInfo['carpool'] = $this->getCarpoolAccount($account);
@@ -115,7 +115,7 @@ class AccountMix extends AccountModel
       $account  = NULL;
       $accountField = 'carpool_account';
       $map = [];
-      $map[] = ['is_delete','<>', 1];
+      $map[] = ['is_delete','=', 0];
       if(!$account_id){
         $account      = isset($params['account']) ? $params['account'] : NULL;
         $type         = isset($params['type']) ? $params['type'] : "carpool";
