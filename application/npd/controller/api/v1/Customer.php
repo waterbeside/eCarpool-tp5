24a7b93189857eec9a3a32d8cf7a241586bf57ba
waterbeside <waterbeside@163.com>
2019-09-20 17:33:04 +0800

    opt: 各户列表添加group筛选

--- application/npd/controller/api/v1/Customer.php
+++ application/npd/controller/api/v1/Customer.php
@@ -6,6 +6,7 @@ use app\api\controller\ApiBase;
 
 use app\npd\model\Customer as CustomerModel;
 use app\npd\model\Category;
+use my\RedisData;
 
 use think\Db;
 
@@ -26,17 +27,25 @@ class Customer extends ApiBase
     /**
      * 取得客户列表
      */
-    public function index($is_recommend = 0, $pagesize = 24)
+    public function index($group = '', $is_recommend = 0, $pagesize = 24)
     {
+        $page = input('param.page', 1);
+        $cacheKey = "NPD:customer:list:g_{$group}_re_{$is_recommend}_page_{$page}_pz_{$pagesize}";
+        $redis = new RedisData();
+        $returnData= $redis->cache($cacheKey);
+        if ($returnData) {
+            $this->jsonReturn(0, $returnData, 'Successful');
+        }
         $map = [
             ['is_delete', '=', 0],
         ];
         if ($is_recommend) {
             $map[] = ['is_recommend', '=', 1];
         }
-
+        if (!empty($group)) {
+            $map[] = $group === 'other' ? ['r_group', 'in', ['', 'other'] ] : ['r_group', '=', $group];
+        }
         $lists_res = CustomerModel::where($map)->order('sort DESC')->paginate($pagesize, false, ['query' => request()->param()]);
-
         $pagination = [
             'total' => $lists_res->total(),
             'page' => input('page', 1),
@@ -47,6 +56,20 @@ class Customer extends ApiBase
         $returnData = [
             'list' => $lists,
             'pagination' => $pagination,
+        ];
+        $redis->cache($cacheKey, $returnData, 60);
+        $this->jsonReturn(0, $returnData, 'Successful');
+    }
+
+
+    /**
+     * 取得分组
+     *
+     */
+    public function group()
+    {
+        $returnData = [
+            'list' => config('npd.customer_group'),
         ];
         $this->jsonReturn(0, $returnData, 'Successful');
     }
