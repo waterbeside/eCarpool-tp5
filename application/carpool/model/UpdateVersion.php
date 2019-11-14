8f51adb727afce99a16c09a18cb7f527498a3480
waterbeside <waterbeside@163.com>
2019-11-14 16:35:11 +0800

    feat: Add findByPlatform methon

--- application/carpool/model/UpdateVersion.php
+++ application/carpool/model/UpdateVersion.php
@@ -3,6 +3,8 @@
 namespace app\carpool\model;
 
 use think\Model;
+use my\RedisData;
+use think\Db;
 
 class UpdateVersion extends Model
 {
@@ -14,4 +16,48 @@ class UpdateVersion extends Model
     protected $connection = 'database_carpool';
 
     protected $pk = 'update_version_id';
+
+    /**
+     * 通过平台查找数据
+     *
+     * @param String  $platform platform
+     * @param Integer $exp 缓存有效时长
+     * @return Array
+     */
+    public function findByPlatform($platform, $exp = 60 * 10)
+    {
+        $cacheKey = "carpool:update_version:{$platform}";
+        $redis = new RedisData();
+        $cacheData = $redis->cache($cacheKey);
+        if ($cacheData) {
+            return $cacheData;
+        }
+        $map = [
+            ['app_id', '=', Db::raw(1)],
+            ['is_delete', '=', Db::raw(0)],
+            ['platform', '=', $platform]
+        ];
+        $res  = $this->where($map)->order('update_version_id DESC')->find();
+        if ($res) {
+            $res = $res->toArray();
+            $cacheData = $redis->cache($cacheKey, $res, $exp);
+            $cacheKey2 = "carpool:update_version:{$platform}:white_list";
+            $cacheData = $redis->cache($cacheKey2, array_filter(explode(',', $res['white_list'])), $exp);
+        }
+        return $res;
+    }
+
+    /**
+     * 清除缓存
+     *
+     * @param String  $platform platform
+     * @return void
+     */
+    public function DeleteCacheByPlatform($platform)
+    {
+        $cacheKey = "carpool:update_version:{$platform}";
+        $cacheKey2 = "carpool:update_version:{$platform}:white_list";
+        $redis = new RedisData();
+        $redis->delete($cacheKey, $cacheKey2);
+    }
 }
