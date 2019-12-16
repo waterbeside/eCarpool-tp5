ee65383176c0511a8f7b6f1963e5972a44ca95c7
waterbeside <waterbeside@163.com>
2019-12-16 16:29:01 +0800

    feat: formatTimeFields method

--- application/common/service/Service.php
+++ application/common/service/Service.php
@@ -22,6 +22,19 @@ class Service
         return false;
     }
 
+    /**
+     * 取得错误信息
+     *
+     * @return array {code,msg,data}
+     */
+    public function getError()
+    {
+        return [
+            'code' => $this->errorCode,
+            'msg' => $this->errorMsg,
+            'data' => $this->data,
+        ];
+    }
 
     public function redis()
     {
@@ -30,5 +43,34 @@ class Service
         }
         $this->redisObj = new RedisData();
         return $this->redisObj;
+    }
+
+
+    /**
+     * 格式化时间字段
+     *
+     * @param array $data 要处理的数据
+     * @param string $dataType 处理的数据格式类型 [list or item]
+     * @param mixed $fields 要处理的字段 可为string||array
+     * @return array
+     */
+    public function formatTimeFields($data, $dataType = 'item', $fields = ['time','create_time'])
+    {
+        if (is_string($fields)) {
+            $fields = explode(',', $fields);
+        }
+
+        if ($dataType == 'list') {
+            foreach ($data as $key => $value) {
+                $data[$key] = $this->formatTimeFields($value, 'item', $fields);
+            }
+        } else {
+            foreach ($fields as $key => $value) {
+                if (isset($data[$value])) {
+                    $data[$value] = strtotime($data[$value]);
+                }
+            }
+        }
+        return $data;
     }
 }
