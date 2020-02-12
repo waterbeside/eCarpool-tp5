8df515daed67d8093b25b96112768602e0dd5a27
waterbeside <waterbeside@163.com>
2020-02-12 16:33:55 +0800

    update

--- application/api/controller/v1/shuttle/Trip.php
+++ application/api/controller/v1/shuttle/Trip.php
@@ -298,9 +298,8 @@ class Trip extends ApiBase
         $returnData['lists'] = $ShuttleTripService->formatTimeFields($returnData['lists'], 'list', ['time','create_time','update_time']);
         foreach ($returnData['lists'] as $key => $value) {
             $timePass = time() - $value['time'];
-            $returnData['lists'][$key]['have_started'] = $timePass >= 0 ? ( $timePass > 20 ? 2 : 1) : 0;
+            $returnData['lists'][$key]['have_started'] = $timePass >= 0 ? ( $timePass > 20 * 60 ? 2 : 1) : 0;
         }
-
         return $this->jsonReturn(0, $returnData, 'Successful');
     }
 
