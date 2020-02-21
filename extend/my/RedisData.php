3559e19ec42cfee126858b803406e449e83dcb55
waterbeside <waterbeside@163.com>
2020-02-21 17:31:28 +0800

    feat: geo相关方法

--- extend/my/RedisData.php
+++ extend/my/RedisData.php
@@ -165,4 +165,131 @@ class RedisData extends Redis
         $cacheKey = "lock:{$lockKey}";
         return $this->del($cacheKey);
     }
+
+    /**
+     * 通过lng和lat计算geo hash
+     *
+     * @param mixed $lng 当为数组时则以[lng,lat]格式传经纬度，否则放经度数据
+     * @param mixed $lat 纬度
+     * @return string 返回geohash
+     */
+    public function getGeohash($lng, $lat = null)
+    {
+        if (is_array($lng)) {
+            $lnglat = $lng;
+            $lng = $lnglat[0];
+            $lat = $lnglat[1] ?? $lat;
+        } else {
+            $lnglat = [$lng, $lat];
+        }
+        if (empty($lng) || empty($lat)) {
+            return false;
+        }
+        $time = time();
+        $cacheKey = "temp:geo:getGeohash:$lng,$lat,$time";
+        $this->geoadd($cacheKey, $lng, $lat, $time);
+        $this->expire($cacheKey, 30);
+        $res = $this->geohash($cacheKey, $time);
+        return $res[0] ?? false;
+    }
+
+    /**
+     * 通过经续度查询直线距离
+     *
+     * @param mixed $lng1 start_lng or [start_lng, start_lat]
+     * @param mixed $lat1 start_lat or [end_lng, end_lat]
+     * @param float $lng2 end_lng or null
+     * @param float $lat2 end_lat or null
+     * @return void
+     */
+    public function getDistance($lng1, $lat1, $lng2 = null, $lat2 = null, $u = 'm')
+    {
+        if (is_array($lng1) && is_array($lat1)) {
+            if (count($lng1) < 2 || count($lat1) < 2) {
+                return false;
+            }
+            $u = !empty($lng2) && is_string($lng2) ? $lng2 : $u;
+            $start = $lng1;
+            $end = $lat1;
+            $lng1 = $start[0];
+            $lat1 = $start[1];
+            $lng2 = $end[0];
+            $lat2 = $end[1];
+        }
+        $cacheKey = "temp:geo:getDistance:$lng1,$lat1,$lng2,$lat2";
+        $this->geoadd($cacheKey, $lng1, $lat1, 'p1', $lng2, $lat2, 'p2');
+        $this->expire($cacheKey, 30);
+        $res = $this->geodist($cacheKey, 'p1', 'p2', $u);
+        return $res;
+    }
+
+    /**
+     * 从列表中找出指定距离范围内的数据
+     *
+     * @param array $list 列表数据
+     * @param array $lnglat [lng,lat]格式传经纬度
+     * @param array $lnglat [lng,lat]经纬度字段名
+     * @return void
+     */
+    public function getRadius($list, $lnglat, $radius = 1000, $u = 'm', $fieldname = ['longitude', 'latitude'], $opt = ['WITHDIST', 'ASC'])
+    {
+        $time = microtime();
+        $rd = $this->getRandomString(6, 0);
+        $cacheKey = "temp:geo:getRadius:{$rd}_{$time}";
+        foreach ($list as $key => $value) {
+            $lngField = $fieldname[0] ?? 'longitude';
+            $latField = $fieldname[1] ?? 'latitude';
+            $lngValue = $value[$lngField] ?? 0;
+            $latValue = $value[$latField] ?? 0;
+            $this->geoadd($cacheKey, $lngValue, $latValue, $key);
+        }
+        $lng = $lnglat[0] ?? 0;
+        $lat = $lnglat[1] ?? 0;
+        $u = $u ?: 'm';
+        $res = $this->georadius($cacheKey, $lng, $lat, $radius, $u, $opt);
+        if ($res) {
+            $newList = [];
+            foreach ($res as $key => $value) {
+                $oListKey = $value[0];
+                if (isset($list[$oListKey])) {
+                    $list[$oListKey]['_radius'] = $value;
+                    $newList[] = $list[$oListKey];
+                }
+            }
+            $list = $newList;
+        }
+        $this->expire($cacheKey, 30);
+        // $this->del($cacheKey);
+        return $list;
+
+    }
+
+    /**
+     * 产生随机字符串
+     * 产生一个指定长度的随机字符串,并返回给用户
+     * @access public
+     * @param int $len 产生字符串的位数
+     * @param int $type 0: 生成数字或字母，1：只生成数字
+     * @return string
+     */
+    public function getRandomString($len = 6, $type = 0)
+    {
+        $chars =  $type ? array(
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        ) : array(
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2",
+            "3", "4", "5", "6", "7", "8", "9"
+        );
+        $charsLen = count($chars) - 1;
+        shuffle($chars);    // 将数组打乱
+        $output = "";
+        for ($i = 0; $i < $len; $i++) {
+            $output .= $chars[mt_rand(0, $charsLen)];
+        }
+        return $output;
+    }
 }
