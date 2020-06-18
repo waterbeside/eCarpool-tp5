797e0de2bb4b498244065b211ada0d0d99d30e27
waterbeside <waterbeside@163.com>
2020-06-18 16:47:04 +0800

    opt: 添加列表缓存

--- application/api/controller/v1/content/Category.php
+++ application/api/controller/v1/content/Category.php
@@ -6,6 +6,7 @@ use think\Db;
 use app\api\controller\ApiBase;
 use app\content\model\Idle as IdleModel;
 use app\content\model\Category as CategoryModel;
+use my\RedisData;
 
 /**
  * 二手市场分类
@@ -26,8 +27,12 @@ class Category extends ApiBase
      */
     public function index()
     {
-        
+        $redis = RedisData::getInstance();
         $type = input('param.type', '');
+        $cacheData = $redis->cache('carpool:category:selects:'.($type ?: 'all'));
+        if ($cacheData) {
+            return $this->jsonReturn(0, ['lists'=>$cacheData], 'success');
+        }
         $fields = "t.id,t.parent_id,t.type,t.sort,t.name_zh,t.name_en,t.name_vi";
         $map = [
             ["t.is_delete", "=", Db::raw(0)],
@@ -36,10 +41,12 @@ class Category extends ApiBase
             $map[] = ['t.type', '=', $type];
         }
         $orderby = "t.sort DESC";
-        $results = CategoryModel::alias('t')->field($fields)->json(['images'])->where($map)->order($orderby)->select();
+        $results = CategoryModel::alias('t')->field($fields)->where($map)->order($orderby)->select();
         if (!$results) {
             $this->jsonReturn(20002, [], 'No Data');
         }
+        $redis->cache('carpool:category:selects:'.($type ?: 'all'), $results, 60 * 60);
+
         $returnData = [
             'lists' => $results
         ];
