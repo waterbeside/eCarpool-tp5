8ca92f6724c2d5ab8d1d1c972b6e99b3de5a920b
waterbeside <waterbeside@163.com>
2020-12-07 10:29:25 +0800

    feat: 通用的添加页和编辑页控制器

--- application/admin/controller/npd/NpdAdminBase.php
+++ application/admin/controller/npd/NpdAdminBase.php
@@ -54,19 +54,57 @@ class NpdAdminBase extends AdminBase
     }
 
     /**
-     * 验证数据有没有权限
+     * 验证所有数据id都是否权限，其中一个没有则返回false
      *
-     * @param array $data 数据
+     * @param object $modelInstance 模型实例
+     * @param array $ids 数据id列表
+     * @param boolean $unGetDeleted 是否排除已删的数据
      * @param integer $resFlag 当为1时，如果没有权限，则直接抛出json；
      * @return boolean
      */
-    public function checkItemSiteAuth($data, $resFlag = 0)
+    public function checkIdsAuth($modelInstance, $ids, $unGetDeleted = false, $resFlag = 0)
     {
         $adminId = $this->userBaseInfo['uid'];
         if ($adminId == 1) {
             return true;
         }
-        $siteId = $data['site_id'];
+        // 先查出所有数据的site_id
+        $where = [
+            ['id', 'in', $ids]
+        ];
+        if ($unGetDeleted) {
+            $where[] = ['is_delete', '=', Db::raw(0)];
+        }
+        $res = $modelInstance->where($where)->column('site_id');
+        if (empty($res)) {
+            return null;
+        }
+        $hasAuth = true;
+        foreach ($res as $item) {
+            if (!$this->checkSiteIdAuth($item, 0)) {
+                $hasAuth = false;
+                break;
+            }
+        }
+        if ($resFlag == 1 && !$hasAuth) {
+            return $this->jsonReturn(-1, '包含没有权限的数据，操作失败');
+        }
+        return $hasAuth;
+    }
+    
+    /**
+     * 验证某个Site_id是否有权限.
+     *
+     * @param integer $siteId 站点id
+     * @param integer $resFlag 当为1时，如果没有权限，则直接抛出json；
+     * @return void
+     */
+    public function checkSiteIdAuth($siteId, $resFlag = 0)
+    {
+        $adminId = $this->userBaseInfo['uid'];
+        if ($adminId == 1) {
+            return true;
+        }
         if (empty($siteId)) {
             return $resFlag == 1 ? $this->jsonReturn(-1, '你没有权限') : false;
         }
@@ -75,6 +113,18 @@ class NpdAdminBase extends AdminBase
             return true;
         }
         return $resFlag == 1 ? $this->jsonReturn(-1, '你没有权限') : false;
+    }
+
+    /**
+     * 验证数据有没有权限
+     *
+     * @param array $data 数据
+     * @param integer $resFlag 当为1时，如果没有权限，则直接抛出json；
+     * @return boolean
+     */
+    public function checkItemSiteAuth($data, $resFlag = 0)
+    {
+        return $this->checkSiteIdAuth($data['site_id'], $resFlag);
     }
 
     /**
@@ -88,7 +138,9 @@ class NpdAdminBase extends AdminBase
         $siteListIdMap = Utils::getInstance()->list2Map($this->authNpdSite['site_list'], 'id');
         foreach ($list as $key => $value) {
             $siteId = $value['site_id'];
-            $list[$key]['site_data'] = $siteListIdMap[$siteId] ?? null;
+            $siteData = $siteListIdMap[$siteId] ?? null;
+            $list[$key]['site_data'] = $siteData;
+            $list[$key]['site_name'] = $siteData['name'] ?? '';
         }
         return $list;
     }
@@ -125,5 +177,105 @@ class NpdAdminBase extends AdminBase
         $siteList = $this->authNpdSite['site_list'];
         $this->siteListIdMap = Utils::getInstance()->list2Map($siteList, 'id');
         return $this->siteListIdMap;
+    }
+
+    /**
+     * 显示编辑页
+     *
+     * @param object $modelInstance 数据模型实例
+     * @param integer $id 数据id
+     * @param string $cateModelName 分类的所属模型名
+     * @param boolean $isFetch 是否直接渲染模板
+     * @return void
+     */
+    public function editPage($modelInstance, $id, $cateModelName, $isFetch = false)
+    {
+        $itemRes = $this->getItemAndCheckAuthSite($modelInstance, $id);
+        if (!$itemRes['auth']) {
+            return '你没有权限';
+        }
+        $data = $itemRes['data'] ?? [];
+        $this->assign('data', $data);
+
+        $this->getCateLevelList($cateModelName, $data['site_id']);
+        if ($isFetch) {
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * 显示添加页
+     *
+     * @param string $cateModelName 分类的所属模型名
+     * @param boolean $isFetch 是否直接渲染模板
+     * @return void
+     */
+    public function addPage($cateModelName, $isFetch = false)
+    {
+
+        $siteId = $this->authNpdSite['site_id'];
+        if (empty($siteId)) {
+            return view('npd/common/select_site');
+        }
+
+        $this->getCateLevelList($cateModelName, $siteId);
+        if ($isFetch) {
+            return $this->fetch();
+        }
+        return null;
+    }
+
+    /**
+     * 取得 category_level_list
+     *
+     * @param string $cateModelName 分类的所属模型名
+     * @param integer $siteId 站点id
+     * @return array
+     */
+    public function getCateLevelList($cateModelName, $siteId)
+    {
+        $category_level_list = $this->getNpdCategoryList($cateModelName, $siteId, false, true, true);
+        $category_level_list = array2level($category_level_list, 0, 1, 'parent_id');
+        $this->assign('category_level_list', $category_level_list);
+        return $category_level_list;
+    }
+
+    /**
+     * 检查权限并册除
+     *
+     * @param object $modelInstance 数据模型实例
+     * @param integer|array $id 数据id
+     * @param boolean $isFetch 是否直接渲染模板
+     * @param string $logFlag 记录日志时语句，为null时不记录
+     * @return void
+     */
+    public function checkAuthAndDelete($modelInstance, $id, $isFetch = false, $logFlag = null)
+    {
+        
+        if (empty($id)) {
+            return $this->jsonReturn(-1, '请选择需要删除的内容');
+        }
+        if (is_array($id)) {
+            $this->checkIdsAuth($modelInstance, $id, true, 1);
+            $where = [['id', 'in', $id]];
+        } else {
+            $this->getItemAndCheckAuthSite($modelInstance, $id, false, 1);
+            $where = [['id', '=', $id]];
+        }
+        $res = $modelInstance->where($where)->update(['is_delete' => 1]);
+        if (!$isFetch) {
+            return $res;
+        }
+        if ($res) {
+            if ($logFlag) {
+                $this->log("{$logFlag}成功", 0);
+            }
+            return $this->jsonReturn(0, '删除成功');
+        } else {
+            if ($logFlag) {
+                $this->log("{$logFlag}失败", -1);
+            }
+            return $this->jsonReturn(-1, '删除失败');
+        }
     }
 }
