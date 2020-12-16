c0b5db910f15dc9ec008e97cd0ed39abfa826dcf
waterbeside <waterbeside@163.com>
2020-12-16 14:15:02 +0800

    feat: 产品推荐分站点权限

--- application/admin/controller/npd/ProductRcm.php
+++ application/admin/controller/npd/ProductRcm.php
@@ -25,20 +25,30 @@ class ProductRcm extends NpdAdminBase
      */
     public function index($filter = ['keyword' => ''])
     {
-        $map   = [];
-        $map[] = ['t.is_delete', '=', Db::raw(0)];
+        $where   = [];
+        $where[] = ['t.is_delete', '=', Db::raw(0)];
+
+        $siteListIdMap = $this->getSiteListIdMap();
+        $siteIdwhere = $this->authNpdSite['sql_site_map'];
+        if (!empty($siteIdwhere)) {
+            $where[] = $siteIdwhere;
+        }
 
         if (isset($filter['keyword']) && $filter['keyword']) {
-            $map[] = ['title|title_en', 'like', "%{$filter['keyword']}%"];
+            $where[] = ['title|title_en', 'like', "%{$filter['keyword']}%"];
         }
 
         if (isset($filter['status']) && is_numeric($filter['status'])) {
-            $map[] = ['status', '=', $filter['status']];
+            $where[] = ['status', '=', $filter['status']];
         }
 
-        $listBef  = ProductRecommend::alias('t')->where($map)->order('t.sort DESC , t.create_time DESC , t.id DESC ');
+        $listBef  = ProductRecommend::alias('t')->where($where)->order('t.sort DESC , t.create_time DESC , t.id DESC ');
         $list =  $listBef->select();
         $total = count($list);
+        foreach ($list as $key => $value) {
+            $siteData = $siteListIdMap[$value['site_id']] ?? [];
+            $list[$key]['site_name'] = $siteData['title'] ?? '';
+        }
         
         $returnData = [
             'filter' => $filter,
@@ -57,6 +67,7 @@ class ProductRcm extends NpdAdminBase
     {
         if ($this->request->isPost()) {
             $data          = $this->request->param();
+            $this->checkItemSiteAuth($data, 1); //检查权限
             $data['model'] = 'product';
             $data['status'] = isset($data['status']) ? isset($data['status']) : 0;
             $validate_result = $this->validate($data, 'app\npd\validate\Recommend');
@@ -65,12 +76,14 @@ class ProductRcm extends NpdAdminBase
             }
             $ProductRecommend = new ProductRecommend();
             if ($ProductRecommend->allowField(true)->save($data)) {
+                $this->log('添加产品推荐成功', 0);
                 $this->jsonReturn(0, '保存成功');
             } else {
+                $this->log('添加产品推荐失败', -1);
                 $this->jsonReturn(-1, '保存失败');
             }
         } else {
-            return $this->fetch('add');
+            return $this->addPage(null, true);
         }
     }
 
@@ -83,23 +96,29 @@ class ProductRcm extends NpdAdminBase
      */
     public function edit($id)
     {
+        $dataModel = new ProductRecommend();
         if ($this->request->isPost()) {
+            $itemRes = $this->getItemAndCheckAuthSite($dataModel, $id);
+            if (!$itemRes['auth']) {
+                $this->jsonReturn(-1, '没有权限');
+            }
             $data          = $this->request->param();
             $data['status'] = isset($data['status']) ? isset($data['status']) : 0;
-            $validate_result = $this->validate($data, 'app\npd\validate\Recommend');
+            $validate_result = $this->validate($data, 'app\npd\validate\Recommend.edit');
             if ($validate_result !== true) {
                 $this->jsonReturn(-1, $validate_result);
             }
-            $ProductRecommend = new ProductRecommend();
-            if ($ProductRecommend->allowField(true)->save($data, $id)) {
+            if ($dataModel->allowField(true)->save($data, $id)) {
+                $this->log('编辑产品推荐成功', 0);
                 $this->jsonReturn(0, '保存成功');
             } else {
+                $this->log('编辑产品推荐失败', -1);
                 $this->jsonReturn(-1, '保存失败');
             }
         } else {
-            $data     = ProductRecommend::find($id);
             $this->assign('id', $id);
-            return $this->fetch('edit', ['data' => $data]);
+            $dataModel = new ProductRecommend();
+            return $this->editPage($dataModel, $id, null, true);
         }
     }
 
@@ -110,12 +129,7 @@ class ProductRcm extends NpdAdminBase
      */
     public function delete($id)
     {
-        if (ProductRecommend::where('id', $id)->update(['is_delete' => 1])) {
-            $this->log('删除产品推荐成功', 0);
-            $this->jsonReturn(0, '删除成功');
-        } else {
-            $this->log('删除产品推荐失败', -1);
-            $this->jsonReturn(-1, '删除失败');
-        }
+        $dataModel = new ProductRecommend();
+        return $this->checkAuthAndDelete($dataModel, $id, true, '删除产品推荐');
     }
 }
--- application/npd/validate/Recommend.php
+++ application/npd/validate/Recommend.php
@@ -8,9 +8,16 @@ class Recommend extends Validate
 {
     protected $rule = [
         'image'  => 'require',
+        'site_id'  => 'require|min:1',
     ];
 
     protected $message = [
         'image.require'     => '请上传图片',
+        'site_id.min'     => '没有选择站点',
+        'site_id.require'  => '没有选择站点',
+    ];
+
+    protected $scene = [
+        'edit'  =>  ['image'],
     ];
 }
